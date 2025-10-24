99bf83cd37a0521408462190a6684f85fdd89271
dipaola-mit <59573328+dipaola-mit@users.noreply.github.com>
2025-10-24 03:39:21 +0000

    Deploying to gh-pages from @ dipaola-mit/dipaola-mit-github.io@91821f114985263b915b0438b9b7b1bb2c81d9fc 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "T",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
